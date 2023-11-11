2823a6938ad87e03e5e4a843a2cba8e30972fcc6
Ben Ross <mrbenross@gmail.com>
2023-11-11 12:19:51 -0600

    axios-imported

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,6 +1,6 @@
 // ❗ You don't need to add extra action creators to achieve MVP
 import { MOVE_CLOCKWISE, MOVE_COUNTERCLOCKWISE,SET_SELECTED_ANSWER,SET_INFO_MESSAGE,SET_QUIZ_INTO_STATE } from "./action-types"
-
+import axios from 'axios';
 
 export function moveClockwise() {
   return({type: MOVE_CLOCKWISE})
@@ -29,6 +29,11 @@ export function resetForm() { }
 // ❗ Async action creators
 export function fetchQuiz() {
   return function (dispatch) {
+    dispatch(setQuiz(null))
+    axios.get('http://localhost:9000/api/quiz/next')
+    .then(res =>{
+      dispatch(setQuiz(res.data))
+    })
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
